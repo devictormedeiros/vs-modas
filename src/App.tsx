e982131671f5cca1509dbf5b36333d7bb8b4a67a
Victor Medeiros <victor.medeiros@seox.com.br>
2023-09-25 23:29:11 -0300

    Add bootstrap e componentização dos estilos sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import './index.scss'
 import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Bem vindos á loja VsModas</h1>
-      <h2>salve</h2>
       <Header />
     </>
   )
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,14 +2,38 @@ import React from "react";
 import "./style.scss";
 
 const Header = () => {
-
-    const styles ={
-        display: "flex",
-    }
-
   return (
-    <header style={styles}>
-      <h1>Header 1</h1>
+    <header>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <a href="/" className="logo">
+              <img src="https://via.placeholder.com/200x50" alt="logotipo da loja" />
+            </a>
+          </div>
+          <div className="col-lg-9">
+          <nav>
+            <ul className="navbar">
+              <li>
+                <a href="/categoria/camisetas">Camisetas</a>
+              </li>
+              <li>
+                <a href="/categoria/calcas">Calças</a>
+              </li>
+              <li>
+                <a href="/categoria/saias">Saias</a>
+              </li>
+              <li>
+                <a href="/categoria/vestidos">Vestidos</a>
+              </li>
+              <li>
+                <a href="/categoria/sapatos">Sapatos</a>
+              </li>
+            </ul>
+          </nav>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
